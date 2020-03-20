f486db99f699b7e9ca1e5d49647263e62a207068
devdigital <devdigital.cloud@gmail.com>
2020-03-20 17:14:37 +0000

    chore: update icon

--- packages/storybook-addon-theme-ui/src/Tool.js
+++ packages/storybook-addon-theme-ui/src/Tool.js
@@ -4,36 +4,25 @@ import { useState } from 'react'
 import { IconButton } from '@storybook/components'
 
 const ThemeUIIcon = ({ ...props }) => (
-  <svg {...props} viewBox="0 0 47.5 47.5">
-    {/* <defs id="defs6">
-      <clipPath id="clipPath18">
-        <path d="M 0,38 38,38 38,0 0,0 0,38 z" id="path20" />
-      </clipPath>
-    </defs> */}
-    <g transform="matrix(1.25,0,0,-1.25,0,47.5)" id="g12">
-      <g id="g14">
-        <g id="g16">
-          <g transform="translate(1,33)" id="g22">
-            <path
-              d="m 0,0 c 0,2.209 1.791,4 4,4 l 28,0 c 2.209,0 4,-1.791 4,-4 l 0,-28 c 0,-2.209 -1.791,-4 -4,-4 l -28,0 c -2.209,0 -4,1.791 -4,4 L 0,0 z"
-              id="path24"
-              style={{
-                fill: '#ffffff',
-                fillOpacity: 1,
-                fillRule: 'nonzero',
-                stroke: '#000000',
-                strokeWidth: 0.72000003,
-                strokeLinecap: 'round',
-                strokeMiterlimit: 4,
-                strokeOpacity: 1,
-                strokeDasharray: '2.88, 5.76',
-                strokeDashoffset: 0,
-                markerStart: 'none',
-              }}
-            />
-          </g>
-          <g transform="translate(16.7681,22.2007)" id="g26" />
-        </g>
+  <svg {...props} viewBox="0 0 54 54">
+    <g transform="matrix(1.25,0,0,-1.25,0,47.5)">
+      <g transform="translate(3,28)">
+        <path
+          d="m 0,0 c 0,2.209 1.791,4 4,4 l 28,0 c 2.209,0 4,-1.791 4,-4 l 0,-28 c 0,-2.209 -1.791,-4 -4,-4 l -28,0 c -2.209,0 -4,1.791 -4,4 L 0,0 z"
+          style={{
+            fill: '#ffffff',
+            fillOpacity: 1,
+            fillRule: 'nonzero',
+            stroke: '#000000',
+            strokeWidth: 0.72000003,
+            strokeLinecap: 'round',
+            strokeMiterlimit: 4,
+            strokeOpacity: 1,
+            strokeDasharray: '2.88, 5.76',
+            strokeDashoffset: 0,
+            markerStart: 'none',
+          }}
+        />
       </g>
     </g>
   </svg>
